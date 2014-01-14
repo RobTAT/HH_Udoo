95afa49d7200de7744d04feec495610882cbc8c3
NooB <fanyuantao@gmail.com>
2014-01-14 15:51:17 +0100

    Create activate_ros.h

---
+++ udoo/script/activate_ros.h
@@ -0,0 +1,7 @@
+source /opt/ros/groovy/setup.bash
+export PATH=$PATH:/opt/ros/groovy/bin
+
+# Specify your work directory here
+ROS_WORKSPACE=/home/ubuntu/groovy_workspace
+# Init your workspace
+source /home/ubuntu/groovy_workspace/setup.sh
