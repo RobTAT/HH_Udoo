955e096199e7596cb2bea04129ee3159079ca122
NooB <fanyuantao@gmail.com>
2014-01-14 15:42:24 +0100

    Create package.sh

---
+++ udoo/script/package.sh
@@ -0,0 +1,19 @@
+sudo apt-get update
+
+# IDE
+sudo apt-get -qq install codeblocks codelite
+
+# OpenCV
+sudo apt-get -qq install libopencv-dev
+
+# Compile Tools
+sudo apt-get -qq install cmake gcc-arm-linux-gnueabi gcc-arm-linux-gnueabihf
+
+# UVC camera driver package
+$sudo apt-get -qq install python-opencv libhighgui2.3 libhighgui-dev ffmpeg libv4l-0 libv4l-dev libxine2 libunicap2 libdc1394-22
+# a GUI tools
+$sudo apt-get -qq install cheese guvcview
+
+# Remote Access
+sudo apt-get install -qq putty tightvncserver xvnc4viewer
+
